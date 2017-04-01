18d8cdf4e36d58763cd3a06a904e180159182727
joe-gilbert <JoeGilbert85@gmail.com>
2017-03-31 22:58:27 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>Joe Gilbert</title>
+
+	<meta name="description" content="Joe Gilbert's Online Presence. See his latest work, skillset, and get to know him.">
+	<meta name="keywords" content="Joe Gilbert,Portfolio,Resume,Mechanicsville,Virginia,Richmond">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="resources/css/style.css?v=1.0">
+
+	<!--[if lt IE 9]>
+		<script src="resources/js/html5shiv.min.js"></script>
+	<![endif]-->
+</head>
+
+<?php  /* Imports all necessary php */
+	include 'resources/includes/functions.php'; ?>
+
+<body>
+
+	<?php  /* Imports all scripts */
+		include 'resources/includes/scripts.php'; ?>
+
+</body>
+</html>
