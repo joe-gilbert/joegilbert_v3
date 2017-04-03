80508b108115b800bc4e9e9827bc02e6230ee812
joe-gilbert <JoeGilbert85@gmail.com>
2017-04-02 22:05:15 -0400

    structure, styles, and functionality

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 <head>
 	<meta charset="utf-8">
 
-	<title>Joe Gilbert</title>
+	<title>Joe Gilbert's Online Presence</title>
 
 	<meta name="description" content="Joe Gilbert's Online Presence. See his latest work, skillset, and all other pertinent information.">
-	<meta name="keywords" content="Joe Gilbert,Portfolio,Resume,Mechanicsville,Virginia,Richmond">
+	<meta name="keywords" content="Joe Gilbert, Portfolio, Resume, Mechanicsville, Virginia, Richmond">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,500,900" rel="stylesheet">
 	<link rel="stylesheet" href="resources/css/style.css?v=1.0">
 
 	<!--[if lt IE 9]>
@@ -24,7 +25,136 @@
 
 <body>
 
+	<div class="inner-container portfolio-lp">
 
+		<header>
+
+			<h1>Joe Gilbert</h1>
+			<h2>Web Developer &amp; Team Leader</h2>
+
+			<p class="learn-more-cta"><a class="learn-more-execute" href="profile">+ More Info</a></p>
+
+			<div class="profile" id="profile">
+				<p>I'm a web developer with eight years of real-world experience in a fast-paced small-team environment. Throughout my career, I've focused on: ecommerce (strategy, development, and analysis), email marketing, and WordPress implementations. For almost three years, I've also been leading a design team's efforts in: site design, product design, and marketing.</p>
+				<p>My strong attention to detail, analysis, and focus make me a valuable addition to any team and scenario.</p>
+			</div>
+
+		</header>
+
+		<main role="main">
+
+			<section class="experience">
+
+				<h2 class="section-title">Experience</h2>
+
+				<h3 class="position-job-title">Web Development &amp; Design Manager</h3>
+				<p class="position-business-title">InspireYourPeople.com</p>
+
+				<p class="position-held-date-range date-range">July 2014 &ndash; Present</p>
+
+				<ul class="position-responsibilities">
+					<li>Manage the development and design team's projects, priorities, and direction</li>
+					<li>Build and maintain the company's websites</li>
+					<li>Ecommerce strategy, development, and implementation</li>
+					<li>Monitor and report on Google Analytics and other web analysis applications</li>
+					<li>Organize and lead constant data-driven site and marketing efforts</li>
+					<li>Help improve team and office environment</li>
+					<li>Assist with art direction and sign-off on all design decisions</li>
+				</ul>
+
+
+				<h3 class="position-job-title">Web Developer &amp; Designer</h3>
+				<p class="position-business-title">InspireYourPeople.com</p>
+
+				<p class="position-held-date-range date-range">June 2009 &ndash; June 2014</p>
+
+				<ul class="position-responsibilities">
+					<li>Build and maintain the company's websites</li>
+					<li>Code emails and help organize email marketing efforts</li>
+					<li>Product design and development</li>
+					<li>Design and track marketing ads for email and websites</li>
+					<li>Ecommerce development, strategy, and maintenance</li>
+				</ul>
+
+			</section> <? /* END .experience */ ?>
+
+
+			<section class="education">
+
+				<h2 class="section-title">Education</h2>
+
+				<h3 class="education-institute">James Madison University</h3>
+				<p class="education-concentration">Major: Web Design &amp; Development</p>
+				<p class="education-concentration">Minor: Computer Science</p>
+				<p class="date-range">2004 &ndash; 2008</p>
+
+			</section> <? /* END .education */ ?>
+
+
+			<section class="skills">
+
+				<h2 class="section-title">Skills</h2>
+
+				<h4 class="skill-title">HTML / CSS / SASS</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="90%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">jQuery</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="70%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">PHP</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="50%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">Ecommerce</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="80%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">WordPress</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="70%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">Project Management</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="80%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">Team Leadership</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="80%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">Google Analytics</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="80%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">Email Marketing</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="70%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">Adobe Photoshop</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="50%"><span class="skill-label">Rockstar</span></div></div>
+
+				<h4 class="skill-title">Adobe Illustrator</h4>
+				<div class="skill-experience-bar"><div class="skill-bar-percent" data-experience-percent="50%"><span class="skill-label">Rockstar</span></div></div>
+
+				<aside class="footnote"><p>* not an exhaustive list, just a quick top-level indicator of experience.</p></aside>
+
+			</section> <? /* END .skills */ ?>
+
+
+			<section class="interest">
+
+				<h2 class="section-title">Interests</h2>
+
+				<ul class="interest-list">
+					<li>Web: development, design, creativity</li>
+					<li>All things technology and Apple</li>
+					<li>Music: playing, listening, admiring</li>
+					<li>Outdoors: kayaking and fishing</li>
+					<li>My beautiful wife and 10-month-old</li>
+				</ul>
+
+			</section> <? /* END .interest */ ?>
+
+		</main>
+
+
+		<footer>
+			<p class="copyright">&copy; <?= date("Y") ?> JoeGilbert.me</p>
+		</footer>
+
+	</div> <? /* END .inner-container .portfolio-lp */ ?>
 
 	<?php  /* Imports all scripts */
 		include 'resources/includes/scripts.php'; ?>
--- resources/includes/scripts.php
+++ resources/includes/scripts.php
@@ -2,14 +2,50 @@
 
 <script>
 
-	<? /* jQuery functions */ ?>
+	/* jQuery functions */
 	$(document).ready(function(){
 
-	}); <? /*  END document ready function */ ?>
+		/* Skill Experience Bar Animation */
+		$('.learn-more-execute').on('click', function(e){
+
+			e.preventDefault();
+
+			var linkClicked = $(this),
+					idToReveal = linkClicked.attr("href"),
+					hrefText = linkClicked.text();
+
+			/* Fade In Section */
+			$('#' + idToReveal).toggle('200');
+
+			/* Change CTA to after click */
+			switch(hrefText){
+				case '+ Less Info':
+					linkClicked.text("+ More Info");
+					break;
+
+				default:
+					linkClicked.text("+ Less Info");
+					break;
+			}
+
+		});
 
 
+		/* Skill Experience Bar Animation */
+		$('.skill-bar-percent').each(function(){
 
-	<? /* Google Analytics Tracking */ ?>
+			var skillBar = $(this);
+
+			skillBar.animate({
+				width: skillBar.data("experience-percent")
+			},6000);
+
+		});
+
+	}); /*  END document ready function */
+
+
+	/* Google Analytics Tracking */
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -17,6 +53,6 @@
 
 	ga('create', 'UA-5194968-1', 'auto');
 	ga('send', 'pageview');
-	<? /* END Google Analytics Tracking */ ?>
+	/* END Google Analytics Tracking */
 
 </script>
